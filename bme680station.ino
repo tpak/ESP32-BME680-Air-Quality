06a58b8dc99a97d07a62bef934d174072b983b17
tpak <tpak@users.noreply.github.com>
2019-12-01 00:24:48 -0700

    basic wifi connecgion working

--- bme680station.ino
+++ bme680station.ino
@@ -1,10 +1,22 @@
 #include <Arduino.h>
-#include <SPI.h>
-#include <Wire.h>
+//#include <SPI.h>
+//#include <Wire.h>
 #include <Adafruit_Sensor.h>
 #include <Adafruit_BME680.h>
 
+#include <WiFi.h>
+#include <WiFiMulti.h>
+
+#include <HTTPClient.h>
+
 /***************************************************************************
+  This is a library for the BME680 gas, humidity, temperature & pressure sensor
+  that will send the reading to an influx database for
+
+  It is based off of the work referenced below.
+  Written by Chris Tirpak 
+  BSD license, all text above must be included in any redistribution
+   
   This is a library for the BME680 gas, humidity, temperature & pressure sensor
 
   Designed specifically to work with the Adafruit BME680 Breakout
@@ -21,37 +33,54 @@
   BSD license, all text above must be included in any redistribution
  ***************************************************************************/
 
-#define BME_SCK 13
-#define BME_MISO 12
-#define BME_MOSI 11
-#define BME_CS 10
 
+//todo add a way to calibrate for this - google around there was an example
 #define SEALEVELPRESSURE_HPA (1013.25)
 
 Adafruit_BME680 bme; // I2C
-//Adafruit_BME680 bme(BME_CS); // hardware SPI
-//Adafruit_BME680 bme(BME_CS, BME_MOSI, BME_MISO,  BME_SCK);
+
+WiFiMulti wifiMulti;
 
 void setup() {
+  
   Serial.begin(115200);
   while (!Serial);
+  // delay while serial realy gets started
+  delay(1000);
   Serial.println();
-  Serial.println(F("BME680 test"));
-  Serial.println();
+  Serial.println(F("bme680station  debug"));
+  Serial.flush();
+  // countdown a brief delay 
+  for(int t = 4; t > 0; t--) {
+        Serial.printf("[SETUP] WAIT %d...\n", t);
+        Serial.flush();
+        delay(1000);
+    }
   
   pinMode(LED_BUILTIN, OUTPUT);
   
+  // configure the BME680 sensor
   if (!bme.begin()) {
     Serial.println("Could not find a valid BME680 sensor, check wiring!");
     while (1);
   }
 
-  // Set up oversampling and filter initialization
+  // BME680 oversampling and filter initialization
   bme.setTemperatureOversampling(BME680_OS_8X);
   bme.setHumidityOversampling(BME680_OS_2X);
   bme.setPressureOversampling(BME680_OS_4X);
   bme.setIIRFilterSize(BME680_FILTER_SIZE_3);
   bme.setGasHeater(320, 150); // 320*C for 150 ms
+
+  // configure at least one wifi AP to selecrt from 
+  // todo will make this configurable, etc 
+  wifiMulti.addAP("boomcacka", "LianneMcVey22");
+  //wifiMulti.addAP("BakerzGuest2G", "GoH00s22");
+
+  if((wifiMulti.run() == WL_CONNECTED)) {
+    printWifiStatus();
+  }
+
 }
 
 void loop() {
@@ -101,4 +130,22 @@ void loop() {
   
   digitalWrite(LED_BUILTIN, LOW);   // turn the LED on (HIGH is the voltage level)
   delay(1000);
+}
+
+void printWifiStatus() {
+    Serial.println();
+    Serial.println(F("Wifi connection info:"));
+    Serial.print(F("SSID: "));
+    Serial.println(WiFi.SSID());
+    Serial.print(F("IP Address: "));
+    Serial.println(WiFi.localIP());
+    Serial.print(F("Subnet Mask: "));
+    Serial.println(WiFi.subnetMask());
+    Serial.print(F("Gateway IP: "));
+    Serial.println(WiFi.gatewayIP());
+    long rssi = WiFi.RSSI();
+    Serial.print(F("RSSI: "));
+    Serial.print(WiFi.RSSI());
+    Serial.println(F(" dBm"));
+    Serial.println();
 }
